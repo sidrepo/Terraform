76933c39340c37b9c99bafbd042dce2355bb96f5
sidrepo <65915996+sidrepo@users.noreply.github.com>
2025-03-30 02:08:08 +0530

    Update instance.tf

--- VPC/instance.tf
+++ VPC/instance.tf
@@ -4,10 +4,6 @@ resource "aws_instance" "web-server" {
     subnet_id = aws_subnet.dev_public_subnet.id
     vpc_security_group_ids = [aws_security_group.Dev_SG.id]
     key_name = "${aws_key_pair.jenkins-key-pair.id}"
-    provisioner "file" {
-        source = "nginx.sh"
-        destination = "/tmp/nginx.sh"
-    }
     provisioner "remote-exec" {
         inline = [
              "sudo yum install nginx"
